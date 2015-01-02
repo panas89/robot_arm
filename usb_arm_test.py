d4832ef5a1c991bfa77028c359b76ed2faead76e
Eoin Woods <eoinwoods@gmail.com>
2015-01-02 14:11:29 +0000

    Correction to use unitest.TestCase.assert methods

--- usb_arm_test.py
+++ usb_arm_test.py
@@ -25,14 +25,12 @@ class TestableArm(usb_arm.Arm):
     def __init__(self):
         self.dev = StubUsb()
 
-    def assert_ctrl_cmd(self, idx, value):
-        cmd, time = self.dev.get_ctrl_commands()[idx]
-        assert cmd == value
-
     def assert_ctrl_cmds(self, commands):
+        ret = True
         for idx in range(len(self.dev.get_ctrl_commands())):
             cmd, time = self.dev.get_ctrl_commands()[idx]
-            assert cmd == commands[idx]
+            ret = ret and cmd == commands[idx]
+        return ret
 
     def get_ctrl_cmd(self, idx):
         return self.dev.get_ctrl_commands()[idx]
@@ -40,27 +38,34 @@ class TestableArm(usb_arm.Arm):
     def get_ctrl_cmds(self):
         return self.dev.get_ctrl_commands()
 
+    def get_ctrl_cmd_list(self):
+        return list(map(lambda cmd: cmd[0], self.dev.get_ctrl_commands()))
+
 class UsbArmTest(unittest.TestCase):
 
     def setUp(self):
         self.arm = TestableArm()
 
-    def test_move(self):
+    def test_move_includes_correct_verb(self):
         self.arm.move(usb_arm.LedOn)
-        self.arm.assert_ctrl_cmd(0, usb_arm.LedOn)
-        self.arm.assert_ctrl_cmds([usb_arm.LedOn, usb_arm.Stop])
+        cmd, time = self.arm.get_ctrl_cmd(0)
+        self.assertEqual(usb_arm.LedOn, cmd)
+
+    def test_move_adds_stop_verb(self):
+        self.arm.move(usb_arm.ShoulderDown)
+        self.assertEqual([usb_arm.ShoulderDown, usb_arm.Stop], self.arm.get_ctrl_cmd_list())
 
     def test_default_move_time(self):
         self.arm.move(usb_arm.ElbowUp)
         cmd0, time0 = self.arm.get_ctrl_cmd(0)
         cmd1, time1 = self.arm.get_ctrl_cmd(1)
-        assert 990 <= (time1 - time0) <= 1010
+        self.assertIn(time1 - time0, range(990, 1010))
 
     def test_specific_move_time(self):
         self.arm.move(usb_arm.ElbowUp, 2)
         cmd0, time0 = self.arm.get_ctrl_cmd(0)
         cmd1, time1 = self.arm.get_ctrl_cmd(1)
-        assert 1090 <= (time1 - time0) <= 2010
+        self.assertIn(time1 - time0, range(1090, 2010))
 
 if __name__ == '__main__':
     unittest.main()
